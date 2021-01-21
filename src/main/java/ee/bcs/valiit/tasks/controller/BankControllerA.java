41d123874080188878fa19c2cbf472fda16a3dfe
Aivar Apsalon <apsalona@gmail.com>
2021-01-21 09:42:48 +0200

    BankController

---
+++ src/main/java/ee/bcs/valiit/tasks/controller/BankControllerA.java
@@ -0,0 +1,31 @@
+package ee.bcs.valiit.tasks.controller;
+
+
+import ee.bcs.valiit.tasks.Employee;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+
+@RequestMapping("bank")
+@RestController
+
+public class BankController {
+    HashMap<String, Integer> accountMap = new HashMap<>();
+
+
+    @PostMapping("create-account")
+    public Integer createAccount(@RequestBody String account) {
+        accountMap.put(account, 0);
+        return accountMap.get(account);
+    }
+
+    @GetMapping("account-number/{nr}")
+    public Integer getAccountNr(@PathVariable("nr") String nr) {
+        return accountMap.get(nr);
+    }
+
+
+
+
+}
